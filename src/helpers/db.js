9d19379288a108bb95d22b5b7264c4864abb1558
oherna20 <orazalhernandez.oh@gmail.com>
2021-04-22 21:29:09 -0500

    Added DB Connection

---
+++ src/helpers/db.js
@@ -0,0 +1,25 @@
+import knex from 'knex'
+
+const db = knex({
+  client: process.env.DB_TYPE,
+  connection: {
+    filename: process.env.DB_FILE,
+  },
+  useNullAsDefault: true,
+})
+
+const createTables = async () => {
+    const notesTableExists = await db.schema.hasTable('notes')
+    if (!notesTableExists) {
+      await db.schema.createTable('notes', (table) => {
+        table.increments('id').primary()
+        table.string('title')
+        table.string('content')
+        table.timestamps()
+      })
+    }
+}
+
+createTables()
+
+export default db
--- src/routes/api/notes.js
+++ src/routes/api/notes.js
@@ -1,27 +1,52 @@
 import { Router } from 'express'
 
+import * as notesService from '../../services/notes'
+import logger from '../../helpers/logger'
+
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'Get All the Notes' })
+router.get('/', async (req, res) => {
+    const notes = await notesService.getAll()
+    res.send(notes)
+  })
+
+  router.post('/', async (req, res) => {
+    const { note: newNote } = req.body
+    if (newNote) {
+        const note = await notesService.add(newNote)
+        if (note.error) {
+            res.status(400)
+          }
+          res.send(note)
+        } else {
+          res.status(400).send({ msg: 'Bad Status' })
+        }
+      })
+
+      router.get('/:id', async (req, res) => {
+        const { id } = req.params
+        const note = await notesService.getById(id)
+  if (note) {
+    res.send(note)
+  } else {
+    logger.warn(`Note ${id} doesn't exist`)
+    res.status(404).send({})
+  }
 })
 
-router.post('/', (req, res) => {
-  res.json({ msg: 'Create a Note' })
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { note: updatedNote } = req.body
+    const response = await notesService.update(id, updatedNote)
+
+  if (response.error) {
+    res.status(400)
+  }
+  res.send(response)
 })
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  res.json({ msg: `Getting note ${id}` })
-})
-
-router.put('/:id', (req, res) => {
-  const { id } = req.params
-  res.json({ msg: `Updating Note ${id}` })
-})
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params
-  res.json({ msg: `Deleting Note ${id}` })
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    res.send(await notesService.remove(id))
 })
 export default router
