c5f499b70372b944ce741ca93135c3166e8d0711
oherna20 <orazalhernandez.oh@gmail.com>
2021-04-23 10:58:46 -0500

    Last Changes

--- src/index.js
+++ src/index.js
@@ -8,16 +8,17 @@ import helmet from 'helmet'
 import logger from './helpers/logger'
 import router from './routes'
 import { notFound, errorHandler } from './helpers/errors'
-import auth from './helpers/auth'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
-app.use(auth.initialize())
 
 app.use(router)
 
@@ -25,5 +26,5 @@ app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () =>
-logger.success(`Application started at http://localhost:${process.env.PORT}`),
+  logger.success(`Application started at http://localhost:${process.env.PORT}`),
 )
